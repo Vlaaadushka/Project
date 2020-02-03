360ea47a34887768f90ebbf16e8f3343f51274b6
16000174 <vladastanis@mail.ru>
2020-02-03 16:32:17 +0300

    3

--- Animal.java
+++ Animal.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Animal {
-    String name;
-    int age;
+    private List<Foool> fooolList = new ArrayList<>();
+    private String name;
+    private int age;
 
-
+    public  Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
--- Foool.java
+++ Foool.java
@@ -1,8 +1,11 @@
 import java.io.Serializable;
 
 public class Foool implements Serializable {
-    String name;
-    int mass;
+    private String name;
+    private int mass;
 
-
+    public  Foool(String name, int age) {
+        this.name = name;
+        this.mass = mass;
+    }
 }
